601336f12dcc5e01220e5e2a130b8b3ad9c8fcf5
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-10-23 19:16:52 +0200

    populated TableView

--- TableViewDemo/TableViewDemo/ViewController.swift
+++ TableViewDemo/TableViewDemo/ViewController.swift
@@ -14,7 +14,26 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    let articles = [
+        Article(name: "Adidas Shoes", price: 10000),
+        Article(name: "Nike Bag", price: 15000),
+        Article(name: "Zalando Tshirt", price: 8000),
+        Article(name: "Adidas Bag", price: 12000),
+        Article(name: "Nike Football", price: 7000)
+    ]
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DetailCell", for: indexPath)
+        let article = articles[indexPath.row]
+        cell.textLabel?.text = article.name
+        cell.detailTextLabel?.text = "\(article.price)"
+        return cell
+    }
 }
 
 struct Article {
